e458d3a6dc4dd9c5c81eed74a7cb2baa900aa573
SubAt0mic <pokemonjunkbox@gmail.com>
2025-09-24 12:26:30 -0400

    runtime asset loading/downloading

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use crate::assets::{get_assets, load_assets};
 use crate::block::blocks::Blocks;
 use crate::block::rotatable::Rotatable;
 use crate::dungeon::door::door::DoorType;
@@ -13,20 +14,17 @@ use crate::network::protocol::play::serverbound::EntityInteractionType;
 use crate::network::run_network::run_network_thread;
 use crate::player::player::Player;
 use crate::types::block_position::BlockPos;
-use crate::utils::hasher::deterministic_hasher::DeterministicHashMap;
 use crate::utils::seeded_rng::{seeded_rng, SeededRng};
 use crate::world::world::World;
 use anyhow::bail;
 use glam::DVec3;
-use include_dir::include_dir;
 use rand::prelude::IndexedRandom;
-use tokio::time::Instant;
 use std::collections::HashMap;
-use std::iter::Inspect;
 use std::time::Duration;
 use tokio::sync::mpsc::error::TryRecvError;
 use tokio::sync::mpsc::unbounded_channel;
 
+mod assets;
 mod world;
 mod player;
 mod dungeon;
@@ -40,6 +38,8 @@ mod constants;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    // todo: either a config file with repo/path or command line args.
+    load_assets("assets", "https://github.com/Big-Dungeons/ClearData/archive/refs/heads/main.zip").await?;
     let (network_tx, network_rx) = unbounded_channel::<NetworkThreadMessage>();
     let (main_tx, mut main_rx) = unbounded_channel::<MainThreadMessage>();
 
@@ -53,30 +53,8 @@ async fn main() -> anyhow::Result<()> {
     let rng_seed: u64 = rand::random();
     SeededRng::set_seed(rng_seed);
 
-    let dungeon_strings = include_str!("dungeon_storage/dungeons.txt")
-        .split("\n")
-        .collect::<Vec<&str>>();
-
-
-    let rooms_dir = include_dir!("src/room_data/");
-
-    // roomdata first digit (the key) is just a list of numbers 0..etc. this could just be a vec with roomid lookups.
-    let room_data_storage: DeterministicHashMap<usize, RoomData> = rooms_dir
-        .entries()
-        .iter()
-        .map(|file| {
-            let file = file.as_file().unwrap();
-
-            let contents = file.contents_utf8().unwrap();
-            let name = file.path().file_name().unwrap().to_str().unwrap();
-            let room_data = RoomData::from_raw_json(contents);
-
-            let name_parts: Vec<&str> = name.split(",").collect();
-            let room_id = name_parts.first().unwrap().parse::<usize>().unwrap();
-
-            (room_id, room_data)
-        })
-        .collect();
+    let dungeon_strings = &get_assets().dungeon_seeds;
+    let room_data_storage = &get_assets().room_data;
 
     let dungeon = Dungeon::from_string(
         dungeon_strings.choose(&mut seeded_rng()).unwrap(),
@@ -158,21 +136,7 @@ fn load_doors_into_world(world: &mut World<Dungeon>) {
     
     // Might be a good idea to make a new format for storing doors so that indexes etc don't need to be hard coded.
     // But this works for now...
-    let door_data: Vec<Vec<Blocks>> = include_str!("door_data/doors.txt")
-        .split("\n")
-        .map(|line| {
-            let mut blocks: Vec<Blocks> = Vec::new();
-
-            for i in (0..line.len() - 1).step_by(4) {
-                let substr = line.get(i..i + 4).unwrap();
-                let state = u16::from_str_radix(substr, 16).unwrap();
-
-                blocks.push(Blocks::from(state));
-            }
-
-            blocks
-        })
-        .collect();
+    let door_data: &Vec<Vec<Blocks>> = &get_assets().door_data;
 
     let door_type_blocks: HashMap<DoorType, Vec<Vec<Blocks>>> = HashMap::from_iter(
         vec![
